fc0fe929622c9d3f3e1681723282cb6167eb0cca
Aaron Groch <ar.groch@gmail.com>
2016-11-02 08:52:30 -0400

    still working on car agency

--- car_agency/main.rb
+++ car_agency/main.rb
@@ -10,10 +10,31 @@ class Vehicle
 
 end
 
+class Customer
+
+	attr_accessor :first_name, :last_name, :age, :city
+
+	def initialize(first_name, last_name, age, city)
+		@first_name = first_name
+		@last_name = last_name
+		@age = age
+		@city = city
+	end
+
+end
+
 if File.zero?("vehicles.txt")
 	@vehicles = []
 else
 	@vehicles = File.open("vehicles.txt", "r"){|from_file| Marshal.load(from_file)}
+	#@vehicles = Marshal.load(File.binread("vehicles.txt"))
+	#both lines do same thing
+end
+
+if File.zero?("customers.txt")
+	@customers = []
+else
+	@customers = Marshal.load(File.binread("customers.txt"))
 end
 
 def main_menu
@@ -21,18 +42,30 @@ def main_menu
 	puts "--------------------------"
 	puts "1. Enter New Vehicles"
 	puts "2. List All Vehicles"
+	puts "3. Enter New Customers"
+	puts "4. List All Customers"
 	puts "0. End Program"
 
 	choice = gets.chomp.to_i
 
 	case choice
 		when 1
+			clear_screen
 			enter_vehicles
 		when 2
+			clear_screen
 			list_vehicles
+		when 3
+			clear_screen
+			enter_customers
+		when 4
+			clear_screen
+			list_customers
 		when 0
+			clear_screen
 			puts "Program terminated."
 		else
+			clear_screen
 			puts "Invalid Selection."
 			main_menu
 	end
@@ -55,6 +88,7 @@ def enter_vehicles
 
 		@vehicles.push(Vehicle.new(year, make, model))
 		File.open("vehicles.txt", "w"){|f| f.write(Marshal.dump(@vehicles))}
+		clear_screen
 	end
 	main_menu
 end
@@ -66,5 +100,42 @@ def list_vehicles
 	main_menu
 end
 
+def enter_customer
+	puts "Enter new Customers. Type 'done' for the First Name when completed."
+
+	first_name = ""
+	while first_name != "done"
+		print "First Name: "
+		first_name = gets.chomp
+		if first_name == "done"
+			break
+		end
+		print "Last Name: "
+		last_name = gets.chomp
+		print "Age: "
+		age = gets.chomp.to_i
+		print "City: "
+		city = gets.chomp
+
+
+		@customers.push(Customer.new(first_name, last_name, age, city))
+		File.open("customers.txt", "w"){|f| f.write(Marshal.dump(@customers))}
+		clear_screen
+	end
+	main_menu
+end
+
+def list_vehicles
+	@vehicles.each_with_index do |vehicle, index|
+		puts "#{index+1}. #{vehicle.make} #{vehicle.model} (#{vehicle.year})"
+	end
+	main_menu
+end
+
+
+
+def clear_screen
+	Gem.win_platform? ? (system "cls") : (system "clear")
+end
 
 main_menu
